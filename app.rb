6cd53287df98c370d41ea0e301d84daa91539cc7
Marion Huebner <mphuebner@gmail.com>
2015-10-15 15:50:41 -0500

    starting integration of flickr api

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 require 'bundler'
 Bundler.require()
 
+before do
+  FlickRaw.api_key       = ENV['FLICKR_KEY']
+  FlickRaw.shared_secret = ENV['FLICKR_SECRET']
+end
+
 # ROUTES
 get '/' do
   erb :index
@@ -31,9 +36,17 @@ get '/shake' do
 end
 
 get '/photography' do
+
+  # id = '136173250@N05'
+  photos = flickr.people.getPhotosOf(:user_id => ENV['FLICKR_USER_ID'])
+
+  # photos = flickr.photosets.getPhotos( :photoset_id => '72157659740260342', :user_id => ENV['FLICKR_USER_ID'] )
+
   erb :photography
 end
 
 get '/this-site' do
   erb :this
 end
+
+
