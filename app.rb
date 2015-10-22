f38cc1feea765198b18a0c04b36c337eb1b01e50
Marion Huebner <mphuebner@gmail.com>
2015-10-22 16:28:39 -0700

    adds aplo - still missing some description

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require 'bundler'
 Bundler.require()
 
-before do
-  FlickRaw.api_key       = ENV['FLICKR_KEY']
-  FlickRaw.shared_secret = ENV['FLICKR_SECRET']
-end
-
 # ROUTES
 get '/' do
   erb :index
@@ -40,12 +35,6 @@ get '/shake' do
 end
 
 get '/photography' do
-
-  # id = '136173250@N05'
-  photos = flickr.people.getPhotosOf(:user_id => ENV['FLICKR_USER_ID'])
-
-  # photos = flickr.photosets.getPhotos( :photoset_id => '72157659740260342', :user_id => ENV['FLICKR_USER_ID'] )
-
   erb :photography
 end
 
