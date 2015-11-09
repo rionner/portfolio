1a824995e52e8d080f96f11bf2d4fe31724f55b8
Marion Huebner <mphuebner@gmail.com>
2015-11-09 09:41:17 -0600

    starts update of photo controller

--- controllers/photo_controller.rb
+++ controllers/photo_controller.rb
@@ -6,12 +6,28 @@ class PhotoController < Sinatra::Application
     FlickRaw.shared_secret = ENV["FLICKR_SECRET"]
   end
 
+  def initialize
+    @photos = {}
+  end
+
+  def add(photos)
+    photos.each do |key, value|
+
+    end
+    self
+  end
+
+  def to_s
+    puts "photo controller - flickr api"
+  end
+
   get '/' do
     @photoset = flickr.photosets.getPhotos :photoset_id => ENV["FLICKR_PORTFOLIO"], :user_id => ENV["FLICKR_USER_ID"], :format => 'json'
 
     @all_photos = @photoset['photo']
-    @photos = []
-    @all_photos.map { | photo | @photos << "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_q.jpg" }
+
+
+    @all_photos.map { | photo | @photos.add('id' => photo['id'], 'small' => "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_m.jpg", 'large' => "https://farm#{photo['farm']}.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_h.jpg") }
 
     erb :photography
   end
