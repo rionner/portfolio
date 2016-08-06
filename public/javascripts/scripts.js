c2231057a971a8830cd9c6af02875106a3cdb5a2
Marion Huebner <mphuebner@gmail.com>
2016-08-06 02:00:33 -0500

    updating stylesheets to SMSCSS & content updates

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,4 +1,3 @@
-
 // document.addEventListener("DOMContentLoaded", function(event) {
 //   var h = document.documentElement.clientHeight;
 
@@ -13,57 +12,40 @@
 
 //   setHeight(h);
 //   // document.getElementsByClassName('quote').style.height = document.documentElement.clientHeight;
-
-
 // });
 
-// $(document).ready(function(){
 
-//   // INDEX
-//   var height = $(window).height();
-//   $('.quote').height(height); // INDEX QUOTE
-//   $('.fill').height(height); // MURRAY IMAGE SIZING
-//   $('.photo-box').height(height); // PHOTOGRAPHY IMAGE SIZING
+$(document).ready(function(){
+  // INDEX
+  var height = $(window).height();
+  $('.img-fill').height(height); // MURRAY IMAGE SIZING
+  $('.photo-box').height(height); // PHOTOGRAPHY IMAGE SIZING
 
-//   $(window).resize(function() {
-//     var height = $(window).height();
-//     $('.quote').height(height); // INDEX QUOTE
-//     $('.fill').height(height); // MURRAY IMAGE SIZING
-//     $('.photo-box').height(height); // PHOTOGRAPHY IMAGE SIZING
-//   });
+  $(window).resize(function() {
+    var height = $(window).height();
+    $('.img-fill').height(height); // MURRAY IMAGE SIZING
+    $('.photo-box').height(height); // PHOTOGRAPHY IMAGE SIZING
+  });
 
-//   $('.sartre').hide();
-//   $('.sinatra').hide();
-//   $('.socrates').delay(1000).fadeOut(1000);
-//   $('.sartre').delay(2000).fadeIn(1500).fadeOut(1000);
-//   $('.sinatra').delay(4500).fadeIn(1500);
+  // PORTFOLIO NAVIGATION SHOW/HIDE
+  $('.expand-nav').on('click', function(){
+    $('.port-nav-list').toggleClass('is-hidden');
+    $('.expand-nav > figure').toggleClass('is-hidden');
+  });
 
-//   // INDEX
-//   // PORTFOLIO SCRIPTS
-//   $('.text').hide();
+  // SHAKE-IT-SPEARE
+  $("#loaded").hide();
+  $("#rewrite").on("click", function(){
+    $("#loaded").show();
+    $("#loading").hide();
+    $.ajax({
+      method: "get",
+      url: "http://shakeitspeare.com/api/sentence",
+      success: function(data){
+        $(".sentence").text(data.sentence);
+      }
+    })
+  });
+});
 
-//   $('.portie-item').mouseover(function() {
-//     if ($('.triangle-topright').length <= 0) $(this).prepend('<div class="triangle-topright"></div>');
-//    });
 
-//   $('.portie-item').mouseleave(function() {
-//     $('.triangle-topright').remove();
-//   });
-
-//   $(".text").fadeIn(600, 'linear');
-
-//   // SHAKE-IT-SPEARE SCRIPTS
-//   $("#loaded").hide();
-//   $("#rewrite").on("click", function(){
-//     $("#loaded").show();
-//     $("#loading").hide();
-//     $.ajax({
-//       method: "get",
-//       url: "http://shakeitspeare.com/api/sentence",
-//       success: function(data){
-//         $(".sentence").text(data.sentence);
-//       }
-//     })
-//   });
-
-// })
