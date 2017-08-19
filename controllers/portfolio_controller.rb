70df47b443fdaf86d863d767d8fabb7d6a83de52
Marion Huebner <mphuebner@gmail.com>
2017-08-19 11:43:22 -0500

    style guide link

--- controllers/portfolio_controller.rb
+++ controllers/portfolio_controller.rb
@@ -8,24 +8,28 @@ class PortfolioController < Sinatra::Application
     erb :aplo
   end
 
-  get '/og6' do
-    erb :og6
+  get '/bill' do
+    erb :murray
   end
 
   get '/marvel' do
     erb :marvel
   end
 
+  get '/og6' do
+    erb :og6
+  end
+
   get '/restaurant' do
     erb :restaurant
   end
 
-  get '/bill' do
-    erb :murray
-  end
-
   get '/shake' do
     erb :shake
+  end
+
+  get '/style-guide' do
+    erb :style_guide
   end
 
   get '/this-site' do
